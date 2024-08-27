8f2a3c02dfc1c7396cfe4c10a4fe9b9cfb021f03
lintanganjar <lintang.lam23@gmail.com>
2024-08-27 08:58:54 +0700

    commit keempat

--- src/app/components/Populer.js
+++ src/app/components/Populer.js
@@ -1,22 +1,22 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link"
-
-const Populer = ({ thumbnail, title, pubDate, description, link }) => {
+const Populer = ({ thumbnail, title, pubDate, link}) => {
     return (
-        <div className="flex border-r border-gray-200"> 
-            <Link href={{
-                pathname: '/Beranda',
-                query: { title, thumbnail, pubDate, description }
-            }} className="flex gap-5 cursor-pointer">
-                <Image className="rounded-xl w-full" src={thumbnail} width={100} height={100} />
+        <>
+         <div className=" flex border-r border-gray-200"> 
+            <Link href={link} className="flex gap-5  cursor-pointer">
+                <Image className="rounded-xl  w-full" src={thumbnail} width ={100} height ={100} />
                 <div className="inline-block text-justify mr-5">
-                    <h2 className="font-bold mb-10">{title}</h2>
+                    <h2 className=" font-bold mb-10">{title}</h2>
                     <p>{pubDate}</p>
                 </div>
             </Link>
+    
         </div>
+    
+        </>
     )
+       
 }
-
-export default Populer;
+export default Populer
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Beranda from "./Beranda/page";
+
 import Post from "./Post/page";
 
 export default function Home(){
     return (
         <>
             <Post></Post>
-            <Beranda></Beranda>
+            
         </>
     )
 }
